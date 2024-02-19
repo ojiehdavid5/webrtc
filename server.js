1d03d7adc3658690f6a51f0bcbb68e1d13859b87
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-02-19 17:07:17 +0100

    changes

--- server.js
+++ server.js
@@ -1,27 +1,24 @@
-const express=require('express');
-const http=require('http');
-const  app=express();
-const server=http.createServer(app);
-const socket=require('socket.io');
+const express = require('express');
+const http = require('http');
+const app = express();
+const server = http.createServer(app);
+const socket = require('socket.io');
 
-const io=socket(server);
-io.on("connnection",(socket) =>{
-    socket.emit('me',socket.id)
-} )
+const io = socket(server);
+io.on('connection', (socket) => { // Corrected event name 'connection'
+    socket.emit('me', socket.id);
+});
 
+socket.on('disconnect', () => {
+    socket.broadcast.emit('callEnded');
+});
 
+socket.on('callUser', (data) => {
+    io.on(data.userToCall).emit('callUser', { signal: data.signalData, from: data.from, name: data.name });
+});
 
-socket.on('disconect',()=>{
-    socket.broadcast.emit('callEnded'); 
-})
+socket.on('answerCall', () => {
+    // Handle the 'answerCall' event
+});
 
-
-socket.on('callUser',(data)=>{
-
-    io.to(data.userToCall).emit('callUser',{signal:data.signalData,from:data.from, name:data.name})
-})
-
-socket.on('answerCall',)
-
-
-server.listen(5000,()=> console.log("server   is running on a port 5000"))
+server.listen(5000, () => console.log("Server is running on port 5000"));
